4beabb209172d48d5a17118246b5d2849c242694
MorlanB <82750591+MorlanB@users.noreply.github.com>
2021-05-20 17:04:36 -0700

    Add project to repository

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace TicTacToe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reset = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // reset
+            // 
+            this.reset.Font = new System.Drawing.Font("Oswald", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reset.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.reset.Location = new System.Drawing.Point(268, 61);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(94, 30);
+            this.reset.TabIndex = 0;
+            this.reset.Text = "RESET";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.b1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.b1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b1.Location = new System.Drawing.Point(103, 3);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(94, 87);
+            this.b1.TabIndex = 1;
+            this.b1.UseVisualStyleBackColor = false;
+            // 
+            // c1
+            // 
+            this.c1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.c1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.c1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.c1.Location = new System.Drawing.Point(203, 3);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(99, 87);
+            this.c1.TabIndex = 2;
+            this.c1.UseVisualStyleBackColor = false;
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.a1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.a1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.a1.Location = new System.Drawing.Point(3, 3);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(94, 87);
+            this.a1.TabIndex = 3;
+            this.a1.UseVisualStyleBackColor = false;
+            // 
+            // a2
+            // 
+            this.a2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.a2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.a2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.a2.Location = new System.Drawing.Point(3, 96);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(94, 87);
+            this.a2.TabIndex = 4;
+            this.a2.UseVisualStyleBackColor = false;
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.b2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.b2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b2.Location = new System.Drawing.Point(103, 96);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(94, 87);
+            this.b2.TabIndex = 5;
+            this.b2.UseVisualStyleBackColor = false;
+            // 
+            // c2
+            // 
+            this.c2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.c2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.c2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.c2.Location = new System.Drawing.Point(203, 96);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(99, 87);
+            this.c2.TabIndex = 6;
+            this.c2.UseVisualStyleBackColor = false;
+            // 
+            // a3
+            // 
+            this.a3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.a3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.a3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.a3.Location = new System.Drawing.Point(3, 189);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(94, 85);
+            this.a3.TabIndex = 7;
+            this.a3.UseVisualStyleBackColor = false;
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.b3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.b3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b3.Location = new System.Drawing.Point(103, 189);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(94, 85);
+            this.b3.TabIndex = 8;
+            this.b3.UseVisualStyleBackColor = false;
+            // 
+            // c3
+            // 
+            this.c3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.c3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.c3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.c3.Location = new System.Drawing.Point(203, 189);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(99, 85);
+            this.c3.TabIndex = 9;
+            this.c3.UseVisualStyleBackColor = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 105F));
+            this.tableLayoutPanel1.Controls.Add(this.a2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.c1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.a3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.b1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.a1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.b2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.c3, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.c2, 2, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 91F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(305, 277);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(162, 119);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(311, 283);
+            this.panel1.TabIndex = 11;
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(638, 467);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.reset);
+            this.Name = "TicTacToe";
+            this.Text = "Tic-Tac-Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class TicTacToe : Form
+    {
+        #region global variables
+        bool turn;
+        Button[,] buttons;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Forma de juego de gato.
+        /// </summary>
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Graphic interface methods
+        /// <summary>
+        /// Se ejecuta al cargar la interfaz gráfica
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttons = new Button[,]
+            {
+                { a1, b1, c1 },
+                { a2, b2, c2 },
+                { a3, b3, c3 }
+            };
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j].FlatAppearance.BorderSize = 0;
+                    buttons[i, j].Click += (sender1, e1) =>
+                    {
+                        string winner;
+                        Button boton = ((Button)sender1);
+                        boton.Text = turn ? "X" : "O";
+                        boton.ForeColor = turn ? Color.FromArgb(255,100,90) : Color.SpringGreen;
+                        boton.Font = new Font(boton.Font.Name, 50);
+
+                        if (HasWon())
+                        {
+                            winner = boton.Text;
+                            MessageBox.Show("Player * has won".Replace("*", boton.Text));
+                            ClearTable();
+                        }
+                        turn = !turn;
+                    };
+                }
+        }
+
+        /// <summary>
+        /// Resetea la tabla desde botón.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClearTable();
+        }
+        #endregion
+
+        #region Business methods
+        /// <summary>
+        /// Resetea la tabla.
+        /// </summary>
+        private void ClearTable()
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j].Text = "";
+                }
+        }
+
+        /// <summary>
+        /// Verifica que el usuario ha ganado iterando sobre un tablero de coordenadas de los botones del gato.
+        /// </summary>
+        /// <returns> bool hasWon : valor que indica si se ha ganado o no. </returns>
+        private bool HasWon()
+        {
+            //Contiene un arreglo de X y un arreglo de Y indicando las posibles combinaciones para ganar.
+            int[,] indices = new int[,]
+           {
+                { 0, 0, 0, 1, 1, 1, 2, 2, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 2, 1, 0 },
+                { 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 0, 0, 1, 1, 1, 2, 2, 2, 0, 1, 2, 0, 1, 2 }
+           };
+            
+            bool hasWon = false;
+            for (int ind = 0; ind < 24; ind += 3)
+            {
+                if( (buttons[indices[0,ind], indices[1, ind]].Text.Equals(buttons[indices[0, ind + 1], indices[1, ind + 1]].Text) &&
+                    buttons[indices[0, ind], indices[1, ind]].Text.Equals(buttons[indices[0, ind + 2], indices[1, ind + 2]].Text)) &&
+                     !String.IsNullOrEmpty(buttons[indices[0,ind], indices[1,ind]].Text) )
+                {
+                    hasWon = true;
+                    break;
+                }
+            }
+            return hasWon;
+        }
+        #endregion
+
+    }
+}
